ad9133ed0b4a45bda28dd9a6057eb03f78051c6e
Shrevijain <shrevijainbhl@gmail.com>
2023-09-09 20:10:15 +0530

    build GUI for compressing file

--- randon.py
+++ randon.py
@@ -1,6 +1,7 @@
 import random
+import PySimpleGUI
 
-lower = int(input("enter lower bound "))
+lower = int(input("enter lower 1bound "))
 upper = int(input("enter upper bound "))
 result = random.randrange(lower, upper+1)
 print(result)
